87f1a7127dc768be45b787dc567613eb6f405b12
fatemaY <fatemayasen2@gmail.com>
2024-01-14 18:10:05 +0200

    user routes and controller

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -3,26 +3,37 @@ import mongoose from "mongoose";
 const userschema = new mongoose.Schema({
     name:{
         type: String,
-        required: true
+        required: [true, "Must provide a Name"],
     },
     gender:{
         type: String,
-        required: true
+        required: true,
+        enum:["male","female"]
+
     },
     age:{
         type: Number,
         required: true,
         min:[15, "your age should be more than 15"]
     },
-    email:{
+    email: {
         type: String,
-        required: true,
-        unique: true
-    },
+        required: [true, "Must provide email"],
+        unique: [true, "This email is already in use"],
+        match: [/^\S+@\S+\.\S+$/, "Please add a valid email"],
+      },
     password:{
         type: String,
         required: true
-    }
+    },
+    // images: [
+    //     {
+    //       type: mongoose.Schema.Types.ObjectId,
+    //       ref: "Image",
+    //     },
+    //   ],
+    
+
 })
 
 
