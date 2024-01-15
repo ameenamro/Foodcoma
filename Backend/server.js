5fab730a830f9ae2c76ea9adcaef5916a12f369e
Nickk91 <nickkaploon@gmail.com>
2024-01-15 10:58:58 +0200

    fix backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,31 +1,29 @@
-import express  from "express";
-import dotenv from "dotenv"
-import cors from "cors"
-import connectDB from "./config/db.js"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
 import { errorHandler } from "./middleware/errorHandling.js";
 import imagerouter from "./routes/image.routes.js";
 
 import userRoutes from "./routes/userRoutes.js";
+import { analyzeImage } from "./controllers/openai.controller.js";
 
-const server=express();
-dotenv.config()
+const server = express();
+dotenv.config();
 
 // midlleware
-server.use(cors())
+server.use(cors());
 server.use(express.json());
-server.use(errorHandler)
+server.use(errorHandler);
 
-// routes 
-server.use("/api/v1/foodcoma",imagerouter);
+// routes
+server.use("/api/v1/foodcoma", imagerouter);
 server.use("/api/v1/foodcama/users", userRoutes);
-
-
 
 const PORT = process.env.PORT || 2000;
 
-connectDB().then(()=>{
-server.listen(PORT, () => {
+connectDB().then(() => {
+  server.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
   });
-
-})
+});
