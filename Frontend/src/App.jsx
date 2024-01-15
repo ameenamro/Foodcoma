18ae49f9a2a92ed18012d4d9c64fc5b4bec8244c
Miraw1206 <144028821+Miraw1206@users.noreply.github.com>
2024-01-15 13:48:01 +0200

    mira

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,41 @@
-import { useState } from "react";
+// import { useState } from "react";
+// import Homepage from "./pages/Homepage/Homepage.jsx";
+// import Footer from "./components/Footer/Footer.jsx";
+// import Navbar from '../Navbar/NavBar.jsx';
+// import "./App.css";
+
+// function App() {
+
+//   return (
+//     <>
+//       <Navbar/>
+//       <Homepage />
+//       <Footer />
+//     </>
+//   );
+// }
+
+// export default App;
+// App.jsx
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "../Navbar/NavBar.jsx";
 import Homepage from "./pages/Homepage/Homepage.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-import "./App.css";
+import Register from "../Navbar/Register.jsx";
+import Login from "../Navbar/Login.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <Homepage />
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
